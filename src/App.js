5c60e73acc63cd748824c2e326e0e113d7d1ad93
Nojus Murauskas <nojus.murauskas@levi9.com>
2023-04-11 17:29:31 +0300

    full cart control with a global context; cool animation on cart button when adding item

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MainNavigation from "./components/Layout/MainNavigation";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import { useState } from "react";
+import CartProvider from "./store/cartProvider";
 
 function App() {
 	const [cartIsShown, setCartIsShown] = useState(false);
@@ -14,14 +15,14 @@ function App() {
 		setCartIsShown(false);
 	};
 	return (
-		<>
+		<CartProvider>
 			{cartIsShown && <Cart onClose={hideCartHandler} />}
 			<MainNavigation onShowCart={showCartHandler} />
 			<main>
 				<Meals />
 				<Card />
 			</main>
-		</>
+		</CartProvider>
 	);
 }
 
